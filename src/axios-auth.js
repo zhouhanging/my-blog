c785b1428eed17fe53dfdf0c74f0b702ff3826fe
zhouhang <3493329756@qq.com>
2026-04-09 00:26:27 +0800

    初始化

--- src/axios-auth.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const instance = axios.create({
-  baseURL:"https://wd1182543348jfzvtq.wilddogio.com"
-})
-
-// instance.defaults.headers.common['SOMETHING'] = 'SOMETHING'
-
-export default instance
